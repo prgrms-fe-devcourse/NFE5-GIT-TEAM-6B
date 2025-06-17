d0213bbaf789c7fc2fdc696d4d0b16cb006855ed
chechoii <chechoii2e@gmail.com>
2025-06-18 08:38:58 +0900

    search- html/css

--- js/data.js
+++ js/data.js
@@ -1,10 +1,10 @@
-const exercises = [
+export const exercises = [
   {
     id: 1,
     name: "데드리프트",
     category: "등",
-    thumbnail: "assets/images/thumbnail1.png",   // 리스트용 썸네일
-    video: "assets/videos/video1.mp4",       // 상세용 영상
+    thumbnail: "assets/images/thumbnail1.png", // 리스트용 썸네일
+    video: "assets/videos/video1.mp4", // 상세용 영상
     desc: "몸 뒤쪽 근육들이 중량을 버티는 데에 많은 관여를 하기 때문에 등 근육 발달에 좋은 운동이에요. 완벽한 등근육을 가지고 싶다면 필수 운동!",
     startPos: [
       "1. 바벨과 3cm 떨어진 곳에 정강이를 위치시켜 주세요.",
@@ -12,14 +12,8 @@ const exercises = [
       "3. 무릎과 고관절을 굽혀 바벨을 어깨너비로 잡아주세요.",
       "4. 가슴을 펴고 등과 하체에 긴장감을 유지해 주세요.",
     ],
-    movement: [
-      "1. 블라블라",
-    ],
-    breathing: [
-
-    ],
-    cautions: [
-
-    ]
+    movement: ["1. 블라블라"],
+    breathing: [],
+    cautions: [],
   },
 ];
---
+++ js/index.js
@@ -0,0 +1,4 @@
+import { exercises } from "./data.js";
+
+const searchInput = document.querySelector("#search_input");
+const cardContainer = document.querySelector(".card_container");
