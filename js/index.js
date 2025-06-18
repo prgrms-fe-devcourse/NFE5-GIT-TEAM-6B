b6f49424c84c85e4be5b830af5e3d45f5d5cc8d0
chechoii <chechoii2e@gmail.com>
2025-06-19 01:49:35 +0900

    ++

--- js/index.js
+++ js/index.js
@@ -1,16 +1,35 @@
-import { exercises } from "./data.js";
+import { exercises } from "../js/data.js";
 
 const searchInput = document.querySelector("#search_input");
 const cardContainer = document.querySelector(".card_container");
 const autocompleteList = document.querySelector(".autocomplete_list");
 
-function createExerciseCard() {
+// 카드 생성 함수
+function createExerciseCard(item) {
   const card = document.createElement("div");
   card.className = "exercise_card";
   card.innerHTML = `
-  <img src="${exercises.thumbnail}" alt="${exercises.name}" />
+  <img src="${item.thumbnail}" alt="${item.name}" />
   `;
   return card;
+}
+
+// 검색 결과 보여주는 함수
+function showSearchResult(keyword) {
+  const matchedExercises = exercises.filter((item) =>
+    item.name.toLowerCase().includes(keyword.toLowerCase())
+  );
+
+  cardContainer.innerHTML = "";
+
+  if (matchedExercises.length > 0) {
+    matchedExercises.forEach((item) => {
+      const card = createExerciseCard(item);
+      cardContainer.appendChild(card);
+    });
+  } else {
+    cardContainer.innerHTML = `<p>해당 이름을 가진 운동을 찾을 수 없어요 😓</p>`;
+  }
 }
 
 function handleSearchInput() {
@@ -39,23 +58,6 @@ function handleSearchInput() {
   });
 
   showSearchResult(inputValue);
-}
-
-function showSearchResult(keyword) {
-  const matchedExercises = exercises.filter((item) =>
-    item.name.toLowerCase().includes(keyword.toLowerCase())
-  );
-
-  cardContainer.innerHTML = "";
-
-  if (matchedExercises.length > 0) {
-    matchedExercises.forEach((item) => {
-      const card = createExerciseCard(item);
-      cardContainer.appendChild(card);
-    });
-  } else {
-    cardContainer.innerHTML = `<p>해당 이름을 가진 운동을 찾을 수 없어요 😓</p>`;
-  }
 }
 
 searchInput.addEventListener("input", handleSearchInput);
