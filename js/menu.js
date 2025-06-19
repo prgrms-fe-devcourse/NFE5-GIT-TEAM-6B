9fba7e588a2b05e6519cbffe90309b278d4f3dd5
ahk0413 <rladkgusgd@gmail.com>
2025-06-19 23:27:47 +0900

    햄버거 버튼 메뉴 수정

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,9 @@
+//햄부기 버튼 클릭시 네비바열기
 const menuToggleBtn = document.querySelector('.menu_toggle_btn');
 const mobileMenu = document.querySelector('.mobile_nav_menu');
 
 menuToggleBtn.addEventListener('click', () => {
-  mobileMenu.classList.toggle('active');
-
+  menuToggleBtn.classList.toggle('active');
 });
 
 const mobileLinks = document.querySelectorAll('.mobile_nav_menu a, .mobile_nav_menu button');
